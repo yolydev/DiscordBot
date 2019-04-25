97afeab76cbd5f152274f93fe30099214c17daad
yolydev <philip.facebook@gmx.at>
2019-04-25 21:41:05 +0200

    -

--- MusicBot/Modules/Profiles/UserProfile.cs
+++ MusicBot/Modules/Profiles/UserProfile.cs
@@ -19,49 +19,20 @@ namespace MusicBot.Modules.Profiles
         private Emoji accountCreated = new Emoji("🔞");
 
         [Command("profile")]
-        public async Task GetUserProfile()
+        public async Task GetUserProfile([Remainder]SocketGuildUser userMention = null)
         {
-            SocketGuildUser user = (SocketGuildUser)Context.User;
-            var userAccount = UserAccounts.GetAccount(user);
+            userMention = userMention ?? (SocketGuildUser)Context.User;
 
-            var b = user.Roles;
-            var c = "";
-            foreach (var roles in b)
-            {
-                if (user.Roles.Count == 1)
-                    c = "None";
-                else
-                    c += roles.Name + "\n";
-            }
-
-            var userAvatar = user.GetAvatarUrl();
-
-            var embed = new EmbedBuilder();
-            embed.WithTitle("**YOUR AWESOME PROFILE**");
-            embed.AddField(userName + " Name", user.Username);
-            embed.AddField(userRoles + " Roles", c.Replace("@everyone", string.Empty), true);
-            embed.AddField(userRep + " Reputation", userAccount.Reputation, true);
-            embed.AddField(accountCreated + " Account created", user.CreatedAt.DayOfWeek + ", " + user.CreatedAt.LocalDateTime);
-            embed.AddField(userJoinedDate + " Joined At", user.JoinedAt.Value.DayOfWeek + ", " +user.JoinedAt.Value.LocalDateTime);
-            embed.WithColor(new Color(237, 61, 125));
-            embed.WithThumbnailUrl(userAvatar);
-
-            await Context.Channel.SendMessageAsync("", false, embed.Build());
-        }
-
-        [Command("profile")]
-        public async Task GetUserProfile([Remainder]SocketGuildUser userMention)
-        {
             var userAccount = UserAccounts.GetAccount(userMention);
 
-            var b = userMention.Roles;
-            var c = "";
-            foreach (var roles in b)
+            var userRoles = userMention.Roles;
+            var allUserRoles = "";
+            foreach (var roles in userRoles)
             {
                 if (userMention.Roles.Count == 1)
-                    c = "None";
+                    allUserRoles = "None";
                 else
-                    c += roles.Name + "\n";
+                    allUserRoles += roles.Name + "\n";
             }
 
             var userAvatar = userMention.GetAvatarUrl();
@@ -69,10 +40,10 @@ namespace MusicBot.Modules.Profiles
             var embed = new EmbedBuilder();
             embed.WithTitle("**YOUR AWESOME PROFILE**");
             embed.AddField(userName + " Name", userMention.Username);
-            embed.AddField(userRoles + " Roles", c.Replace("@everyone", string.Empty), true);
+            embed.AddField(this.userRoles + " Roles", allUserRoles.Replace("@everyone", string.Empty), true);
             embed.AddField(userRep + " Reputation", userAccount.Reputation, true);
-            embed.AddField(accountCreated + " Account created", userMention.CreatedAt.UtcDateTime + " UTC");
-            embed.AddField(userJoinedDate + " Joined At", userMention.JoinedAt.ToString().Replace("+00:00", "UTC"));
+            embed.AddField(accountCreated + " Account created", userMention.CreatedAt.DayOfWeek + ", " + userMention.CreatedAt.LocalDateTime);
+            embed.AddField(userJoinedDate + " Joined At", userMention.JoinedAt.Value.DayOfWeek + ", " + userMention.JoinedAt.Value.LocalDateTime);
             embed.WithColor(new Color(237, 61, 125));
             embed.WithThumbnailUrl(userAvatar);
 
